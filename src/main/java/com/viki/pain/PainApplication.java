41b2b6b140211a7c037dd7998827a4f3995c5736
chenyf <chenyf@fenqi.im>
2019-10-16 16:31:01 +0800

    init

---
+++ src/main/java/com/viki/pain/PainApplication.java
@@ -0,0 +1,13 @@
+package com.viki.pain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PainApplication.class, args);
+    }
+
+}
